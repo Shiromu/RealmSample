618f75b8f5514dc38f0db4fab4d9f01b78c403db
Arai <wataaya1023@gmail.com>
2018-11-04 15:53:35 +0900

    Initial Commit

---
+++ RealmSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmSample
+//
+//  Created by Shiromu on 2018/11/04.
+//  Copyright © 2018 Shiromu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
